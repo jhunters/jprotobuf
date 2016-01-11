77afd80e5181a4f5131de6d4531483d9e3dc982d
xiemalin <xiemalin@baidu.com>
2016-01-11 17:29:56 +0800

    Add maven plugin to supports pre-compile feature while compile JProtobuf pojo classes

---
+++ jprotobuf-precompile-plugin/src/main/java/com/baidu/jprotobuf/mojo/JprotobufPreCompileMain.java
@@ -0,0 +1,85 @@
+package com.baidu.jprotobuf.mojo;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+import com.baidu.bjf.remoting.protobuf.ProtobufProxy;
+import com.baidu.bjf.remoting.protobuf.annotation.Protobuf;
+import com.baidu.bjf.remoting.protobuf.utils.FieldUtils;
+import com.baidu.bjf.remoting.protobuf.utils.JDKCompilerHelper;
+import com.baidu.bjf.remoting.protobuf.utils.compiler.JdkCompiler;
+
+import jodd.io.findfile.ClassScanner;
+
+public class JprotobufPreCompileMain {
+
+	public static void main(String[] args) {
+
+		if (args == null || args.length == 0 || args.length != 3) {
+			throw new RuntimeException(printUsage());
+		}
+		
+		final File outputPath = new File(args[0] + File.separator + "temp");
+		try {
+			FileUtils.deleteDirectory(outputPath);
+		} catch (Exception e) {
+		}
+		outputPath.mkdirs();
+		
+		JDKCompilerHelper.setCompiler(new JdkCompiler(Thread.currentThread().getContextClassLoader()));
+		
+		final String filterClassPackage = args[2];
+
+		ClassScanner scanner = new ClassScanner() {
+
+			@Override
+			protected void onEntry(EntryData entryData) throws Exception {
+				String name = entryData.getName();
+				if (filterClassPackage != null) {
+					if (!name.startsWith(filterClassPackage)) {
+						return;
+					}
+				}
+				
+				Class c = getByClass(name);
+				if (c == null) {
+					return;
+				}
+
+				try {
+					List<Field> fields = FieldUtils.findMatchedFields(c, Protobuf.class);
+					if (!fields.isEmpty()) {
+						ProtobufProxy.create(c, false, outputPath);
+					}
+				} catch (Throwable e) {
+				}
+			}
+		};
+
+		scanner.scanDefaultClasspath();
+		
+		// copy files
+		try {
+			FileUtils.copyDirectory(outputPath, new File(args[1]));
+		} catch (IOException e) {
+		}
+
+	}
+
+	private static String printUsage() {
+		return "Usage: " + JprotobufPreCompileMain.class.getName() + " outputFile";
+	}
+
+	private static Class getByClass(String name) {
+		try {
+			return Thread.currentThread().getContextClassLoader().loadClass(name);
+		} catch (Throwable e) {
+		}
+		return null;
+	}
+
+}
