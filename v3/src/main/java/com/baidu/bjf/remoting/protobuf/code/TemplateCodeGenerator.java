564979547c3a66e3a1e11ba13bc4531cff862982
xiemalin <xiemalin@BDSYHF124145.internal.baidu.com>
2019-03-06 15:42:52 +0800

    remove used import

--- v3/src/main/java/com/baidu/bjf/remoting/protobuf/code/TemplateCodeGenerator.java
+++ v3/src/main/java/com/baidu/bjf/remoting/protobuf/code/TemplateCodeGenerator.java
@@ -21,7 +21,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.logging.Level;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
