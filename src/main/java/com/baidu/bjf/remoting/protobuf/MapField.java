daa381aacdf6ac975403b364df77b06d3b3b7568
xiemalin <xiemalin@baidu.com>
2021-03-01 13:43:25 +0800

    add comments

--- src/main/java/com/baidu/bjf/remoting/protobuf/MapField.java
+++ src/main/java/com/baidu/bjf/remoting/protobuf/MapField.java
@@ -1,4 +1,18 @@
-// Protocol Buffers - Google's data interchange format
+/*
+ * Copyright 2002-2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 // Copyright 2008 Google Inc.  All rights reserved.
 // https://developers.google.com/protocol-buffers/
 //
@@ -49,6 +63,9 @@ import com.google.protobuf.Message;
  * 
  * THREAD-SAFETY NOTE: Read-only access is thread-safe. Users can call getMap() and getList() concurrently in multiple
  * threads. If write-access is needed, all access must be synchronized.
+ *
+ * @param <K> the key type
+ * @param <V> the value type
  */
 public class MapField<K, V> {
     /**
@@ -64,45 +81,121 @@ public class MapField<K, V> {
      * As the map field changes its mode, the list/map reference returned in a previous method call may be invalidated.
      */
     private enum StorageMode {
-        MAP, LIST, BOTH
+
+        /** The map. */
+        MAP,
+        /** The list. */
+        LIST,
+        /** The both. */
+        BOTH
     }
 
+    /** The mode. */
     private volatile StorageMode mode;
+
+    /** The map data. */
     private Map<K, V> mapData;
+
+    /** The list data. */
     private List<Message> listData;
 
+    /**
+     * The Interface Converter.
+     *
+     * @param <K> the key type
+     * @param <V> the value type
+     */
     // Convert between a map entry Message and a key-value pair.
     private static interface Converter<K, V> {
+
+        /**
+         * Convert key and value to message.
+         *
+         * @param key the key
+         * @param value the value
+         * @return the message
+         */
         Message convertKeyAndValueToMessage(K key, V value);
 
+        /**
+         * Convert message to key and value.
+         *
+         * @param message the message
+         * @param map the map
+         */
         void convertMessageToKeyAndValue(Message message, Map<K, V> map);
 
+        /**
+         * Gets the message default instance.
+         *
+         * @return the message default instance
+         */
         Message getMessageDefaultInstance();
     }
 
+    /**
+     * The Class ImmutableMessageConverter.
+     *
+     * @param <K> the key type
+     * @param <V> the value type
+     */
     private static class ImmutableMessageConverter<K, V> implements Converter<K, V> {
+
+        /** The default entry. */
         private final MapEntry<K, V> defaultEntry;
 
+        /**
+         * Instantiates a new immutable message converter.
+         *
+         * @param defaultEntry the default entry
+         */
         public ImmutableMessageConverter(MapEntry<K, V> defaultEntry) {
             this.defaultEntry = defaultEntry;
         }
 
+        /*
+         * (non-Javadoc)
+         * 
+         * @see com.baidu.bjf.remoting.protobuf.MapField.Converter#convertKeyAndValueToMessage(java.lang.Object,
+         * java.lang.Object)
+         */
         public Message convertKeyAndValueToMessage(K key, V value) {
             return defaultEntry.newBuilderForType().setKey(key).setValue(value).buildPartial();
         }
 
+        /*
+         * (non-Javadoc)
+         * 
+         * @see
+         * com.baidu.bjf.remoting.protobuf.MapField.Converter#convertMessageToKeyAndValue(com.google.protobuf.Message,
+         * java.util.Map)
+         */
         public void convertMessageToKeyAndValue(Message message, Map<K, V> map) {
             MapEntry<K, V> entry = (MapEntry<K, V>) message;
             map.put(entry.getKey(), entry.getValue());
         }
 
+        /*
+         * (non-Javadoc)
+         * 
+         * @see com.baidu.bjf.remoting.protobuf.MapField.Converter#getMessageDefaultInstance()
+         */
         public Message getMessageDefaultInstance() {
             return defaultEntry;
         }
     }
 
+    /** The converter. */
     private final Converter<K, V> converter;
 
+    /**
+     * Instantiates a new map field.
+     *
+     * @param converter the converter
+     * @param mode the mode
+     * @param mapData the map data
+     * @param listData the list data
+     */
     private MapField(Converter<K, V> converter, StorageMode mode, Map<K, V> mapData, List<Message> listData) {
         this.converter = converter;
         this.mode = mode;
@@ -110,29 +203,70 @@ public class MapField<K, V> {
         this.listData = listData;
     }
 
+    /**
+     * Instantiates a new map field.
+     *
+     * @param defaultEntry the default entry
+     * @param mode the mode
+     * @param mapData the map data
+     * @param listData the list data
+     */
     private MapField(MapEntry<K, V> defaultEntry, StorageMode mode, Map<K, V> mapData, List<Message> listData) {
         this(new ImmutableMessageConverter<K, V>(defaultEntry), mode, mapData, listData);
     }
 
-    /** Returns an immutable empty MapField. */
+    /**
+     * Returns an immutable empty MapField.
+     *
+     * @param <K> the key type
+     * @param <V> the value type
+     * @param defaultEntry the default entry
+     * @return the map field
+     */
     public static <K, V> MapField<K, V> emptyMapField(MapEntry<K, V> defaultEntry) {
         return new MapField<K, V>(defaultEntry, StorageMode.MAP, Collections.<K, V> emptyMap(), null);
     }
 
-    /** Creates a new mutable empty MapField. */
+    /**
+     * Creates a new mutable empty MapField.
+     *
+     * @param <K> the key type
+     * @param <V> the value type
+     * @param defaultEntry the default entry
+     * @return the map field
+     */
     public static <K, V> MapField<K, V> newMapField(MapEntry<K, V> defaultEntry) {
         return new MapField<K, V>(defaultEntry, StorageMode.MAP, new HashMap<K, V>(), null);
     }
 
+    /**
+     * Convert key and value to message.
+     *
+     * @param key the key
+     * @param value the value
+     * @return the message
+     */
     private Message convertKeyAndValueToMessage(K key, V value) {
         return converter.convertKeyAndValueToMessage(key, value);
     }
 
+    /**
+     * Convert message to key and value.
+     *
+     * @param message the message
+     * @param map the map
+     */
     @SuppressWarnings("unchecked")
     private void convertMessageToKeyAndValue(Message message, Map<K, V> map) {
         converter.convertMessageToKeyAndValue(message, map);
     }
 
+    /**
+     * Convert map to list.
+     *
+     * @param mapData the map data
+     * @return the list
+     */
     private List<Message> convertMapToList(Map<K, V> mapData) {
         List<Message> listData = new ArrayList<Message>();
         for (Map.Entry<K, V> entry : mapData.entrySet()) {
@@ -141,6 +275,12 @@ public class MapField<K, V> {
         return listData;
     }
 
+    /**
+     * Convert list to map.
+     *
+     * @param listData the list data
+     * @return the map
+     */
     private Map<K, V> convertListToMap(List<Message> listData) {
         Map<K, V> mapData = new HashMap<K, V>();
         for (Message item : listData) {
@@ -149,7 +289,11 @@ public class MapField<K, V> {
         return mapData;
     }
 
-    /** Returns the content of this MapField as a read-only Map. */
+    /**
+     * Gets the map.
+     *
+     * @return the map
+     */
     public Map<K, V> getMap() {
         if (mode == StorageMode.LIST) {
             synchronized (this) {
@@ -162,7 +306,11 @@ public class MapField<K, V> {
         return Collections.unmodifiableMap(mapData);
     }
 
-    /** Gets a mutable Map view of this MapField. */
+    /**
+     * Gets the mutable map.
+     *
+     * @return the mutable map
+     */
     public Map<K, V> getMutableMap() {
         if (mode != StorageMode.MAP) {
             if (mode == StorageMode.LIST) {
@@ -174,15 +322,28 @@ public class MapField<K, V> {
         return mapData;
     }
 
+    /**
+     * Merge from.
+     *
+     * @param other the other
+     */
     public void mergeFrom(MapField<K, V> other) {
         getMutableMap().putAll(MapFieldLite.copy(other.getMap()));
     }
 
+    /**
+     * Clear.
+     */
     public void clear() {
         mapData = new HashMap<K, V>();
         mode = StorageMode.MAP;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
     @SuppressWarnings("unchecked")
     @Override
     public boolean equals(Object object) {
@@ -193,17 +354,30 @@ public class MapField<K, V> {
         return MapFieldLite.<K, V> equals(getMap(), other.getMap());
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
     @Override
     public int hashCode() {
         return MapFieldLite.<K, V> calculateHashCodeForMap(getMap());
     }
 
-    /** Returns a deep copy of this MapField. */
+    /**
+     * Returns a deep copy of this MapField.
+     *
+     * @return the map field
+     */
     public MapField<K, V> copy() {
         return new MapField<K, V>(converter, StorageMode.MAP, MapFieldLite.copy(getMap()), null);
     }
 
-    /** Gets the content of this MapField as a read-only List. */
+    /**
+     * Gets the list.
+     *
+     * @return the list
+     */
     List<Message> getList() {
         if (mode == StorageMode.MAP) {
             synchronized (this) {
@@ -216,7 +390,11 @@ public class MapField<K, V> {
         return Collections.unmodifiableList(listData);
     }
 
-    /** Gets a mutable List view of this MapField. */
+    /**
+     * Gets the mutable list.
+     *
+     * @return the mutable list
+     */
     List<Message> getMutableList() {
         if (mode != StorageMode.LIST) {
             if (mode == StorageMode.MAP) {
@@ -229,7 +407,9 @@ public class MapField<K, V> {
     }
 
     /**
-     * Gets the default instance of the message stored in the list view of this map field.
+     * Gets the map entry message default instance.
+     *
+     * @return the map entry message default instance
      */
     Message getMapEntryMessageDefaultInstance() {
         return converter.getMessageDefaultInstance();
